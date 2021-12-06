302e0c6cd7d112b2d394446a6f17a59ec4085b2a
kangyunsik <re12io@ajou.ac.kr>
2021-12-06 15:22:51 +0900

    인터셉터 등록하고, excludePathPatterns를 사용하여 error 페이지 관련 처리 (실제 사용자가 보낸 요청이 아닌 것) 컨트롤러를 등록하여, 해당 흐름은 처리하지 않도록 설정

--- src/main/java/hello/exception/WebConfig.java
+++ src/main/java/hello/exception/WebConfig.java
@@ -1,9 +1,11 @@
 package hello.exception;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
@@ -11,6 +13,15 @@ import javax.servlet.Filter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico","/error","/error-page/**");
+        // Error에 대한 정보가 아닌 /error-page/ 하위의 컨트롤러에는 적용하지 않도록 설정
+    }
 
     @Bean
     public FilterRegistrationBean logFilter(){
